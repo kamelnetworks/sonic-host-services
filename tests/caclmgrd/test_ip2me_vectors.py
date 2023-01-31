7dccbaa49101d0a11338a8970171a5e289445399
Christian Svensson <blue@cmd.nu>
2023-01-31 16:11:22 +0100

    kamel: [caclmgrd] Use interface IP for IP2ME
    Currently the first IP on the VLAN subnet is used, regardless of
    whatever IP is actually assigned to the control plane. This fix uses the
    correct IP.
    
    See earlier work:
     - https://github.com/sonic-net/sonic-buildimage/pull/9826
     - https://github.com/sonic-net/sonic-buildimage/pull/7178
     - https://github.com/sonic-net/sonic-buildimage/issues/7008
    
    Signed-off-by: Christian Svensson <blue@cmd.nu>

--- tests/caclmgrd/test_ip2me_vectors.py
+++ tests/caclmgrd/test_ip2me_vectors.py
@@ -86,6 +86,32 @@ CACLMGRD_IP2ME_TEST_VECTOR = [
         },
     ],
     [
+        "One VLAN interface, /24, we are .2",
+        {
+            "config_db": {
+                "MGMT_INTERFACE": {
+                    "eth0|172.18.0.100/24": {
+                        "gwaddr": "172.18.0.1"
+                    }
+                },
+                "LOOPBACK_INTERFACE": {},
+                "VLAN_INTERFACE": {
+                    "Vlan110|10.10.11.2/24": {},
+                },
+                "PORTCHANNEL_INTERFACE": {},
+                "INTERFACE": {},
+                "DEVICE_METADATA": {
+                    "localhost": {
+                    }
+                },
+                "FEATURE": {},
+            },
+            "return": [
+                ["iptables", "-A", "INPUT", "-d", "10.10.11.2/32", "-j", "DROP"],
+            ],
+        },
+    ],
+    [
         "One interface of each type, IPv6, /64 - block all interfaces but MGMT",
         {
             "config_db": {
@@ -114,10 +140,10 @@ CACLMGRD_IP2ME_TEST_VECTOR = [
             },
             "return": [
                 ['ip6tables', '-A', 'INPUT', '-d', '2001:db8:10::/128', '-j', 'DROP'],
-                ['ip6tables', '-A', 'INPUT', '-d', '2001:db8:11::1/128', '-j', 'DROP'],
+                ['ip6tables', '-A', 'INPUT', '-d', '2001:db8:11::/128', '-j', 'DROP'],
                 ['ip6tables', '-A', 'INPUT', '-d', '2001:db8:12::/128', '-j', 'DROP'],
                 ['ip6tables', '-A', 'INPUT', '-d', '2001:db8:13::/128', '-j', 'DROP']
-            ],  
+            ],
         },
     ]
 ]
